d95e826d50eb6b69187252436ce1d0965bd750a5
Jamie McDonald <jdamcd@gmail.com>
2015-04-24 17:12:11 -0400

    Support iOS reviews

--- app.rb
+++ app.rb
@@ -1,5 +1,6 @@
 require 'sinatra'
 require 'supermarket'
+require 'net/http'
 
 require 'rack-cache'
 require 'dalli'
@@ -19,9 +20,28 @@ before do
 	cache_control :public, max_age: 1800  # 30 min
 end
 
-get '/tv/:app_id' do |app_id|
+get '/android/:app_id' do |app_id|
 	opts = {'login' => ENV["GOOGLE_LOGIN"], 'password' => ENV["GOOGLE_PASSWORD"]}
 	session = Supermarket::Session.new(opts)
-	@results = session.comments(app_id, 0, 10).comments_list.to_a
+	
+	@results = session.comments(app_id, 0, 10).comments_list.to_a.map { |c| Comment.new(c.text, c.rating) }
 	erb :comments
+end
+
+get '/ios/:app_id' do |app_id|
+	response = Net::HTTP.get("itunes.apple.com", "/us/rss/customerreviews/id=" + app_id + "/json")
+	reviews = JSON.parse(response)["feed"]["entry"]
+
+	@results = reviews[1,10].map { |r| Comment.new(r["content"]["label"], r["im:rating"]["label"]) }
+	erb :comments
+end
+
+class Comment
+	attr_accessor :text
+	attr_accessor :rating
+
+	def initialize(text, rating)
+		@text = text
+		@rating = rating
+	end
 end
